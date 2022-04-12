a5deaae285991ac518ef5e17fdd2f2b6c7345872
rauibert <rauibert@Rauibert.rauibert>
2022-04-13 01:27:43 +0200

    Probando estilos con Chakra UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import {useState, useEffect} from "react";
 import * as API from "./services/swpeople";
+import logo from "./assets/logo.png";
+import { Heading } from '@chakra-ui/react';
 
 export default function App() {
   const [people, setPeople] = useState([]);
@@ -11,10 +13,11 @@ export default function App() {
 
   return (
   <>
-    <h1>Personajes de Star Wars</h1>
+    <img src={logo} width="200"/>
+    <Heading as="h1">Personajes de Star Wars</Heading>
     <ul>
-      {people.map((person, i) => (
-        <li key={i}>
+      {people.map((person) => (
+        <li key={person.name}>
         {person.name} {person.birth_year}  
         </li>
       ))}
