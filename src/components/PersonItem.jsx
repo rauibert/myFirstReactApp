a92a0bd6e3024a4d931d66343e3aa24e8c08fdea
rauibert <rauibertnf@gmail.com>
2022-04-23 19:12:19 +0200

    Creado el link

--- src/components/PersonItem.jsx
+++ src/components/PersonItem.jsx
@@ -1,5 +1,6 @@
 import { Box, Flex, Text, Button } from '@chakra-ui/react';
 import { GoPerson, GoCalendar } from "react-icons/go";
+import { Link } from "react-router-dom";
 
 export function PersonItem(person){
     return (
@@ -13,7 +14,10 @@ export function PersonItem(person){
           
           <Flex align="center"><GoPerson /><Text ml="2">Nombre: {person.name}</Text></Flex> 
           <Flex align="center"><GoCalendar /><Text ml="2">Fecha nacimiento: {person.birth_year} </Text></Flex> 
-          <Button colorScheme='teal' size='sm'> Más detalles</Button>          
+          <Link to={`/people/${person.name}`}>
+            <Button colorScheme='teal' size='sm'> Más detalles</Button> 
+          </Link>
+                   
         </Box>
     )
 }
