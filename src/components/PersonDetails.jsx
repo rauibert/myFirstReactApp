64a3d1ac91f300f35c4f52f249456558709c9eef
rauibert <rauibertnf@gmail.com>
2022-04-23 21:52:54 +0200

    Maquetando pagina de detalle

--- src/components/PersonDetails.jsx
+++ src/components/PersonDetails.jsx
@@ -1,6 +1,7 @@
 import { useParams } from "react-router-dom";
 import * as API from "../services/swpeople";
 import {useState, useEffect} from "react";
+import { Box, Flex, Text } from '@chakra-ui/react';
 
 export function PersonDetails(){
     const {peopleid} = useParams();
@@ -15,8 +16,22 @@ export function PersonDetails(){
     
     return (
         
-        <div>
-            <pre>{JSON.stringify(people)}</pre>
-        </div>
+        <Box 
+           
+          bg='gray.100' 
+          p={4} 
+          m={4} 
+          borderRadius={5}
+        >
+          
+          <Flex align="center"><Text ml="2">Nombre: {people.name}</Text></Flex> 
+          <Flex align="center"><Text ml="2">Fecha nacimiento: {people.birth_year} </Text></Flex> 
+          <Flex align="center"><Text ml="2">Altura: {people.height}</Text></Flex> 
+          <Flex align="center"><Text ml="2">Peso: {people.mass} </Text></Flex>
+          <Flex align="center"><Text ml="2">Color de ojos: {people.eye_color}</Text></Flex> 
+          <Flex align="center"><Text ml="2">Color de pelo: {people.hair_color} </Text></Flex>
+          
+                   
+        </Box>
     )
 }
